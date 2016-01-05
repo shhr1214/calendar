b0e4d4b07689e32f96f3d2246c1a4d56a2599e12
shhr1214 <souhei0079@gmail.com>
2016-01-05 22:49:20 +0000

    a lot of changes.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
-
   def new
     @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: '登録に成功しました' }
+     #   format.json {}
+      else
+     #   format.html
+     #   format.json
+      end
+    end
   end
 end
