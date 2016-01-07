aebea64a82f3291445c937ff0355324bda5baf81
shhr1214 <souhei0079@gmail.com>
2016-01-07 23:35:52 +0000

    lots of changes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
   def new
     @user = User.new
+  end
+
+  def index
+    @users = User.all
   end
 
   def create
@@ -8,12 +14,34 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: '登録に成功しました' }
-     #   format.json {}
+        format.html { redirect_to @user, notice: 'User was successfully created!!' }
+        #format.json {}
       else
-     #   format.html
-     #   format.json
+        #format.html
+        #format.json
       end
     end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :employee_id, :password, :auth)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resource :events
-  resource :users
+  resources :events
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
