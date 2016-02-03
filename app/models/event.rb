df68e6ccc6cab97ffb81d32ab83ba787d5172cc7
shhr1214 <souhei0079@gmail.com>
2016-02-03 23:30:37 +0000

    add validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,11 @@
 class Event < ActiveRecord::Base
   has_many :participants
   has_many :users, through: :participants
+
+  validates :title, presence: true
+  validates :start_time, presence: true
+  validates :end_time, presence: true
+  validates :description, presence: true
+  validates :place, presence: true
+  validates :author_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
   has_many :participants
   has_many :events, through: :participants
+
+  validates :name, presence: true
+  validates :employee_id, presence: true
+  validates :password, presence: true
+  validates :auth, presence: true
+  validates :entry_date, presence: true
+  validates :retire_date, presence: true
 end
